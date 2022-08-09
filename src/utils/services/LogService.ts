cab92634c153c93a08b96e3efa0c271d7c8fb76c
AlexeyTsutsoev <imistersin@gmail.com>
2022-08-09 19:18:06 +0200

    docs(services): added JSDocs for Alert and log services

--- src/utils/services/LogService.ts
+++ src/utils/services/LogService.ts
@@ -1,5 +1,14 @@
+/**
+ * Service for logs error
+ */
 export default class LogService {
+  /**
+   * Func for logs errors
+   * @param text string
+   * @param error any error
+   */
   static logError(text: string, error: any) {
     console.log(text, error);
+    // add analalitics if need it...
   }
 }
